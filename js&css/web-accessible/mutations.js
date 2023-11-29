d670a45006c0928e65de0535abe1d5ba64d15047
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-11-29 21:55:59 +0100

    Update mutations.js

--- js&css/web-accessible/mutations.js
+++ js&css/web-accessible/mutations.js
@@ -6,21 +6,16 @@ var ImprovedTube;
 >>> MUTATIONS
 ----------------------------------------------------------------
 # Media element
-	# Play
+	Play()
 --------------------------------------------------------------*/
 
 /*--------------------------------------------------------------
 # MEDIA ELEMENT
 --------------------------------------------------------------*/
 
-/*--------------------------------------------------------------
-# PLAY
---------------------------------------------------------------*/
-
-HTMLMediaElement.prototype.play = (function (original) {
-	return function () {
-		document.dispatchEvent(new CustomEvent('it-play'));
-
-		return original.apply(this, arguments);
-	}
+/** Inject code to dispatch a `it-play` event every time the `play` method is called on any media element in the DOM (e.g., a video element) */
+HTMLMediaElement.prototype.play = (original => function play() {
+	'use strict';
+	document.dispatchEvent(new CustomEvent('it-play'));
+	return original.apply(this, arguments);
 })(HTMLMediaElement.prototype.play);
